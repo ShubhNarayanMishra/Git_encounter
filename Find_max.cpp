17a10d8a395bbc084e561aa5b0cd1d4b87ea2f1b
Shubh Narayan Mishra <m.n.shubh88@gmail.com>
2021-10-03 00:43:52 +0530

    Initial commit

---
+++ Find_max.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+ using namespace std;
+ int main()
+ {
+     int num1,num2,num3;
+     cout<<"Enter Three Numbers"<<endl;
+     cin>>num1;
+     cin>>num2;
+     cin>>num3;
+     if(num1>num2)
+     {
+        if(num1>num3)
+          {
+            cout<<num1;
+          } 
+        else
+             cout<<num3;    
+     }
+     else 
+     {
+         if(num2>num3)
+          {
+            cout<<num2;
+          } 
+        else
+             cout<<num3; 
+     }
+     return 0;
+ }
